d95742a9562080fe0a16282c3e911a181c202f95
Jon Prentice <jon77p@gmail.com>
2019-12-25 22:11:54 -0600

    Modified to set BUILD_DATE through Heroku API

--- backend/deploy.sh
+++ backend/deploy.sh
@@ -1,3 +1,5 @@
 #!/usr/bin/env sh
 
-export HEROKU_BUILD_DATE="${date}"
+HEROKU_BUILD_DATE="$(date)"
+
+curl -nX PATCH -H "Authorization: Bearer $HEROKU_API_TOKEN" -H "Accept: application/vnd.heroku+json; version=3" -H "Content-Type: application/json" https://api.heroku.com/apps/jonprentice-me/config-vars -d "{\"BUILD_DATE\": \"$HEROKU_BUILD_DATE\"}"
